c3e4264f4f978248d8ab42cc2b29ed8c4646fb20
x0n4d0 <romagnolimauricio@gmail.com>
2021-11-07 18:40:48 -0300

    refactor: define the types using Task model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTaks() {
+  getAllTaks(): Task[] {
     return this.tasksService.getAllTaks();
   }
 }
