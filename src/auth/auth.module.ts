b8e33b4777b509e812085980057b09e359190943
x0n4d0 <romagnolimauricio@gmail.com>
2021-11-08 02:29:26 -0300

    chore: exports JwtStrategy and PassportModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+
 import { UsersRepository } from './users.repository';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -17,7 +19,8 @@ import { AuthController } from './auth.controller';
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
