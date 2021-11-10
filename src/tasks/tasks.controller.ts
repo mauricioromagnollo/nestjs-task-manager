0a11cc227a01ff20b4e544b39376911d523283be
x0n4d0 <romagnolimauricio@gmail.com>
2021-11-09 23:52:28 -0300

    refactor: add relative imports

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -16,8 +16,8 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { GetUser } from 'src/auth/get-user.decorator';
-import { User } from 'src/auth/user.entity';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/user.entity';
 
 @Controller('/tasks')
 @UseGuards(AuthGuard())
